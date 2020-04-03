83a3c20cf6ca678a3957c012e0d404996ed66e39
Ali <46216799+althomi@users.noreply.github.com>
2020-04-03 19:33:42 +0200

    different DOM-Manipulations Buttons
    worked myself thorugh different Eventlistener Actions with Buttons and styled 4 Buttons in different ways on UI by interaction

---
+++ resultsTemplate3/assign1.js
@@ -0,0 +1,32 @@
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
+
+
+button1.addEventListener("click", function () {
+    button1.style.backgroundColor="#0537CC";
+});
+
+button2.addEventListener("mouseover", function () {
+    button2.style.backgroundColor="#0537CC";
+});
+
+button3.addEventListener("mouseover", function () {
+    button3.style.backgroundColor="#0537CC";
+
+});
+button3.addEventListener("mouseout", function () {
+    button3.style.backgroundColor="black";
+});
+
+button4.addEventListener("mouseover", function () {
+    button4.style.borderStyle="solid";
+    button4.style.borderColor="#0537CC";
+    button4.style.color="#0537CC";
+})
+button4.addEventListener("mouseout", function () {
+    button4.style.borderStyle="none";
+    button4.style.color="white";
+})
+
